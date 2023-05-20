6d13aea164531a0cbb46436147171e2a78a4ae63
salman_rahman <salmanrahman.cse@gmail.com>
2023-05-20 17:38:39 +0600

    home component

--- resources/js/components/auth/Login.vue
+++ resources/js/components/auth/Login.vue
@@ -66,7 +66,10 @@
     methods:{
         login(){
             axios.post('/api/auth/login',this.form)
-            .then(res=> User.responseAfterLogin(res))
+            .then(res=> {
+                User.responseAfterLogin(res)
+                this.$router.push({name:'home'})
+            })
             .catch(error=> console.log(error.response.data))
         }
     }
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,9 +2,11 @@
 let Login=require('./components/auth/Login.vue').default;
 let Register=require('./components/auth/Register.vue').default;
 let Forget=require('./components/auth/Forget.vue').default;
+let home=require('./components/home.vue').default;
 
 export const routes = [
   { path: '/', component: Login, name:'/' },
   { path: '/register', component: Register , name:'/register' },
-  { path: '/forget', component: Forget , name:'/forget' }
+  { path: '/forget', component: Forget , name:'/forget' },
+  { path: '/home', component: home , name:'home' }
 ]
