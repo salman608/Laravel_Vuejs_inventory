3d7f0e05efc15a381c49b5e5af2e1b77e144b9fc
salman_rahman <salmanrahman.cse@gmail.com>
2023-05-20 11:55:25 +0600

    jwt madilware

--- app/Http/Middleware/JWT.php
+++ app/Http/Middleware/JWT.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class JWT
 {
@@ -16,6 +17,7 @@ class JWT
      */
     public function handle(Request $request, Closure $next)
     {
+        JWTAuth::parseToken()->authenticate();
         return $next($request);
     }
 }
